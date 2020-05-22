74d3aaf77fd815a364137ac300d1f2afcf52e0c6
vinayak <vsg13feb01@gmail.com>
2020-05-22 15:17:41 +0530

    connected to mailchimp server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const request = require("request");
 // const _ = require("lodash");
 const app = express();
 app.set('view engine', 'ejs');
@@ -17,13 +18,50 @@ app.post("/",function(req,res){
   var lastName = req.body.lName;
   var email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  var data ={
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options ={
+    url:"https://us18.api.mailchimp.com/3.0/lists/YOUR_LIST_KEY",
+    method:"POST",
+    headers:{
+      "Authorization":"superman YOUR_API_KEY"
+    },
+    body: jsonData
+  }
+
+  request(options,function(error, response, body){
+    if(error){
+      res.render("failure");
+    } else{
+      if(response.statusCode === 200){
+        res.render("success");
+      }else{
+        res.render("failure");
+      }
+    }
+  });
+
 });
 
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+})
 app.listen(3000, function() {
 
   console.log("Server started on port 3000");
 
 });
-
-//63f7966fb283c9381c2f124737067784-us18
